2a3647717ac3454514b9b27a0e7626b5445aad7e
Robles05 <roberthorvath1990@gmail.com>
2018-05-15 13:58:30 +0200

    robofriends application initial commit

---
+++ src/robots.js
@@ -0,0 +1,32 @@
+export const robots = [
+	{
+		id: 1,
+		name: 'Josh Josh',
+		username: 'Jos',
+		email: 'josh@josh.com',
+	},
+	{
+		id: 2,
+		name: 'Ashley Ashley',
+		username: 'Ash',
+		email: 'ashley@ashley.com',
+	},
+	{
+		id: 3,
+		name: 'George George',
+		username: 'Georg',
+		email: 'george@george.com',
+	},
+	{
+		id: 4,
+		name: 'Laura Laura',
+		username: 'Lau',
+		email: 'laura@lau.com',
+	},
+	{
+		id: 5,
+		name: 'Sarah Sarah',
+		username: 'Shr',
+		email: 'sarah@shr.com',
+	},
+]
